76c453ba5519fe17d5f538f69e78b814a6f53e15
caohuilin <caohuilin@bytedance.com>
2021-11-16 22:57:03 +0800

    feat: init packages

---
+++ .changeset/lovely-carrots-know.md
@@ -0,0 +1,6 @@
+---
+"@modern-js-test/module-test-1": patch
+"@modern-js-test/module-test-2": patch
+---
+
+feat: init
---
+++ packages/module-test-2/src/index.ts
@@ -0,0 +1,5 @@
+import module1 from '@modern-js-test/module-test-1';
+
+export default function () {
+  return `${module1()}_module-2`;
+}
